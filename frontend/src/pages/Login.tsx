730b0a7af7e7413ea8040b383fd9ada8f6037461
Osama Ibrahim <osarezk119@gmail.com>
2026-03-25 19:32:34 +0200

    fix error

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -16,7 +16,7 @@ const Login: React.FC = () => {
     setLoading(true);
 
     try {
-      const API_URL = import.meta.env.VITE_API_URL || '';
+      const API_URL = "https://backend-portfolio-one-theta.vercel.app";
       const res = await fetch(`${API_URL}/api/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
